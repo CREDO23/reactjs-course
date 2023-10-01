17fb24c1d9ae1a072699bd9d40624ed2eff85019
CREDO23 <thierrybakera12@gmail.com>
2023-10-01 22:40:27 +0200

    feat: add button component

--- 3-Interactivity/Quiz-app/src/app.jsx
+++ 3-Interactivity/Quiz-app/src/app.jsx
@@ -1,3 +1,10 @@
-export default function App () {
-    return <h3>Todo App</h3>
+import Button from "./components/shared/button";
+
+export default function App() {
+  return (
+    <>
+      <Button>Enoyer</Button> <Button type="secondary">Send</Button>
+      <Button background={"#0880d6"}>Add</Button>
+    </>
+  );
 }
